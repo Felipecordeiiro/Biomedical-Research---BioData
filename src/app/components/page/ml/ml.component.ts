7dba5df8ea458230412fefeb9adce4a01cfae5c7
Felipe Cordeiro <imfelipe.cordeiro12@gmail.com>
2023-05-31 08:28:35 -0300

    sucess drawline

--- src/app/components/page/ml/ml.component.ts
+++ src/app/components/page/ml/ml.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-ml',
@@ -6,29 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./ml.component.css']
 })
 
-export class MLComponent {
+export class MLComponent implements OnInit, AfterViewInit{
 
-  private canvas = document.getElementById("canvas") as HTMLCanvasElement;
-  private ctx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
-  private lastClick: [number, number] | null = null;
-  private firstLine: [number, number, number, number] | null = null;
-  private image = new Image(600, 300);
+  @ViewChild("canvas", { static: true }) canvasRef!: ElementRef<HTMLCanvasElement>;
+  private ctx: CanvasRenderingContext2D | null = null;
+  private image!: HTMLImageElement;
 
-  constructor() {
-    this.image.onload = () => {
-      this.canvas.width = this.image.width;
-      this.canvas.height = this.image.height;
-      this.ctx.drawImage(this.image, 0, 0, this.canvas.width, this.canvas.height);
-    };
-    this.image.src = "/../../../../assets/pes.jpg";
-    this.canvas.addEventListener("click", this.handleClick.bind(this));
+  ngOnInit() {
   }
 
-  private drawLine(xImg1: number, yImg1: number, xImg2: number, yImg2: number): void {
+  ngAfterViewInit(){
+    const canvas = this.canvasRef.nativeElement;
+    this.ctx = canvas.getContext("2d")
+
+    this.image = new Image(350, 350);
+    this.image.src = "../../../../../assets/pes.jpg";
+    this.image.onload = () => {
+      canvas.width = this.image.width;
+      canvas.height = this.image.height;
+      if(this.ctx){
+        this.ctx.drawImage(this.image, 0, 0, canvas.width, canvas.height);
+      }
+    };
+  }
+
+  drawLine(xImg1: number, yImg1: number, xImg2: number, yImg2: number) {
+    if (!this.ctx) return;
     this.ctx.strokeStyle = "red";
     this.ctx.lineWidth = 2;
-    const scaleX = this.canvas.width / this.image.width;
-    const scaleY = this.canvas.height / this.image.height;
+    const scaleX = this.canvasRef.nativeElement.width / this.image.width;
+    const scaleY = this.canvasRef.nativeElement.height / this.image.height;
     const x1 = xImg1 * scaleX;
     const y1 = yImg1 * scaleY;
     const x2 = xImg2 * scaleX;
@@ -40,16 +47,7 @@ export class MLComponent {
     this.ctx.stroke();
   }
 
-  private calculateAngle(
-    x1: number,
-    y1: number,
-    x2: number,
-    y2: number,
-    x3: number,
-    y3: number,
-    x4: number,
-    y4: number
-  ): void {
+  calculateAngle(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, x4: number, y4: number) {
     const vector1 = [x2 - x1, y2 - y1];
     const vector2 = [x4 - x3, y4 - y3];
     const prodInter = vector2[0] * vector1[0] + vector2[1] * vector1[1];
@@ -57,10 +55,17 @@ export class MLComponent {
     const magnitude2 = Math.sqrt(vector2[0] * vector2[0] + vector2[1] * vector2[1]);
     const angleInRadians = Math.acos(prodInter / (magnitude1 * magnitude2));
     const angleInDegrees = angleInRadians * (180 / Math.PI);
-    console.log(`Ângulo formado entre as duas linhas: ${angleInDegrees} graus`);
+
+    const angleElement = document.getElementById("angle");
+    if (angleElement) {
+      angleElement.textContent = angleInDegrees.toFixed(2) + " graus";
+    }
   }
 
-  private handleClick(event: MouseEvent): void {
+  private lastClick: [number, number] | null = null;
+  private firstLine: [number, number, number, number] | null = null;
+
+  handleClick(event: MouseEvent) {
     const x = event.offsetX;
     const y = event.offsetY;
 
